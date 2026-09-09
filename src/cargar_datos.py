3e2bbde7ff33461327f80bdf1394a81cb00b0e1d
Cristobal-Espinosa <cristo.espinosa4@gmail.com>
2026-09-09 19:37:19 -0300

    Add compiled bytecode for cargar_datos module

--- src/cargar_datos.py
+++ src/cargar_datos.py
@@ -1,6 +1,10 @@
+import os
 import numpy as np
 
-datos = np.genfromtxt("../data/dolar_observado_sii_2022_2025.csv", delimiter=",", skip_header=1, dtype=None, encoding="utf-8")
+carpeta = os.path.dirname(os.path.abspath(__file__))
+ruta_csv = os.path.join(carpeta, "..", "data", "dolar_observado_sii_2022_2025.csv")
+
+datos = np.genfromtxt(ruta_csv, delimiter=",", skip_header=1, dtype=None, encoding="utf-8")
 
 years = datos["f0"]
 meses = datos["f1"]
