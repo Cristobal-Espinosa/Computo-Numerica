5067cf5ed94a2aacf049794856dfa17b7ed44cea
Cristobal-Espinosa <cristo.espinosa4@gmail.com>
2026-09-09 19:27:19 -0300

    elimina archivos obsoletos y agrega nueva implementación de cálculo de deriva y carga de datos

--- src/cargar_datos.py
+++ src/cargar_datos.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-datos = np.genfromtxt("dolar_observado_sii_2022_2025.csv", delimiter=",", skip_header=1, dtype=None, encoding="utf-8")
+datos = np.genfromtxt("../data/dolar_observado_sii_2022_2025.csv", delimiter=",", skip_header=1, dtype=None, encoding="utf-8")
 
 years = datos["f0"]
 meses = datos["f1"]
